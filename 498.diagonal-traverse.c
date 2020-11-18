38b4d5ac39e944ebfedf134b0814a3e3e3c759c9
adgiczone <yunuoki@gmail.com>
2020-11-18 21:32:48 +0800

    start 498

---
+++ 498.diagonal-traverse.c
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=498 lang=c
+ *
+ * [498] Diagonal Traverse
+ */
+
+// @lc code=start
+
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* findDiagonalOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSize){
+
+}
+// @lc code=end
+
