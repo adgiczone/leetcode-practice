dab21848307497bd0e94cd5d4d1f65d360e50037
adgiczone <yunuoki@gmail.com>
2020-11-18 21:32:17 +0800

    add 66 sulotion

---
+++ 66.plus-one.c
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode id=66 lang=c
+ *
+ * [66] Plus One
+ */
+
+// @lc code=start
+#include <stdio.h>
+#include <string.h>
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* plusOne(int* digits, int digitsSize, int* returnSize)
+{
+    if (digitsSize <= 0) {
+        return NULL;
+    }
+    int *recvArray = (int*)malloc(sizeof(int) * (digitsSize + 1));
+    if (!recvArray) {
+        return NULL;
+    }
+    int plus = 1, realSize = digitsSize;
+    for (int i = digitsSize; i > 0; i--) {
+        recvArray[i] = (digits[i - 1] + plus) % 10;
+        plus = (digits[i - 1] + plus) / 10; 
+    }
+
+    if (!recvArray[1] && plus) {
+        recvArray[0] = 1;
+        realSize++;
+    }
+    int *returnArray = (int*) malloc(realSize * sizeof(int));
+    if (!returnArray) return NULL;
+
+    if (realSize!=digitsSize) {
+        memcpy(returnArray, recvArray, sizeof(int) * realSize);
+    } else {
+        memcpy(returnArray, recvArray + 1, sizeof(int) * realSize);
+    }
+    free(recvArray);
+    if (!returnSize) {
+        returnSize = (int*)malloc(sizeof(int));
+        if (!returnSize) return NULL;
+    }
+    (*returnSize) = realSize;
+
+    return returnArray;
+}
+// @lc code=end
+
