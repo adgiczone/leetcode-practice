19bbaba2df8808a9a7f1bf2bb20c2e74547825a3
adgiczone <yunuoki@gmail.com>
2020-12-08 20:03:11 +0800

    498 success

--- 498.diagonal-traverse.c
+++ 498.diagonal-traverse.c
@@ -27,7 +27,7 @@ int* findDiagonalOrder(int** matrix, int matrixSize, int* matrixColSize, int* re
     int col = (*matrixColSize);
     *returnSize = row * col;
     printf("row:%d col:%d \r\n", row, col);
-    int max = (row > col) ? row : col;
+    int max = row + col;//(row > col) ? row : col;
     int* returnArray = (int *)malloc((*returnSize) * sizeof(int));
     if (!returnArray) {
         (*returnSize) = 0;
@@ -40,71 +40,29 @@ int* findDiagonalOrder(int** matrix, int matrixSize, int* matrixColSize, int* re
         if (i < col && j < row) {
             returnArray[index] = matrix[j][i];
             index++;
+            if (index > (*returnSize)) {
+                break;
+            }
         }
-        printf ("i %d j %d %d %d %d\r\n", i, j, returnArray[index - 1], col_minus, plus);
-        if (i % 2 == 0 && j == 0 && !plus) {
-            printf ("in i\r\n");
+        if (i % 2 == 0 && j == 0 ) {
             col_minus = true;
-            plus = true;
             i++;
             continue;
         }
-        else if (j % 2 != 0 && i == 0 && !plus) {
-            printf ("in j\r\n");
+        else if (j % 2 != 0 && i == 0) {
             col_minus = false;
-            plus = true;
             j++;
             continue;
         }else {
-            plus = false;
         }
         if (col_minus) {
             (i-1) < 0 ?  : (i--);
             j++;
-            if (j > )
         } else {
             i++;
             (j-1) < 0 ? : (j--);
         }
     }
-    // while (index < (*returnSize)) {
-    //     returnArray[index] = matrix[i][j];
-    //     printf("returnArray[%d]:%d\n", index, returnArray[index]);
-    //     printf("before i:%d j:%d turn_flag:%d left_minus:%d \r\n", i, j, turn_flag, left_minus);
-    //     if (i == 0 || i == (col - 1) || j == 0 || j == (row-1)) {
-    //         if (turn_flag) {
-    //              printf("%d %d\r\n", (i == 0 || j == 0) && (i != (row - 1)) ? true: false, 
-    //                                 (i == 0 || j == 0) && (j != (col - 1)) ? true: false);
-    //             if (((i == 0 || j == 0) && (i != (row - 1))) && (j != (col - 1))) {
-    //                 (i > j) ? (i++) : (j++);
-    //             } else {
-    //                 (i > j) ? (j++) : (i++);
-    //             }
-
-    //             if ((i >= row) || (j >= col)) {
-    //             }else {
-    //                 turn_flag = false;
-    //             }
-    //         } else {
-    //             turn_flag = true;
-    //         }
-
-    //         left_minus = (i > j) ? true : false;
-    //     }
-
-    //     if (turn_flag == true) {
-    //         if (left_minus) {
-    //             i-1 < 0 ?: i--;
-    //             j+1 >= col ?: j++;
-    //         } else {
-    //             j-1 < 0 ?: j--;
-    //             i+1 >= row ?: i++;
-    //         }
-    //     }
-
-    //     printf("after i:%d j:%d turn_flag:%d left_minus:%d \r\n", i, j, turn_flag, left_minus);
-    //     index++;
-    // }
     return returnArray;
 }
 // @lc code=end
