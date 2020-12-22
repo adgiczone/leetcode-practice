30d4575f5101d22d83be614dc3c5c8febe5d5b80
adgiczone <yunuoki@gmail.com>
2020-12-22 17:52:20 +0800

    start 67 session

---
+++ 67.add-binary.c
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=67 lang=c
+ *
+ * [67] Add Binary
+ */
+
+// @lc code=start
+
+
+char * addBinary(char * a, char * b)
+{
+    int a_len = strlen(a);
+    int b_len = strlen(b);
+    int max_len = a_len > b_len ? a_len : b_len;
+    int min_len = a_len > b_len ? b_len : a_len;
+    int carry = 0;
+
+}
+// @lc code=end
+
