871170fe73e0b84699cc28afa72babfe9652f069
adgiczone <yunuoki@gmail.com>
2020-12-22 20:31:40 +0800

    optimize 67

--- 67.add-binary.c
+++ 67.add-binary.c
@@ -10,32 +10,20 @@ char * addBinary(char * a, char * b)
 {
     int a_len = strlen(a);
     int b_len = strlen(b);
-    char *min = NULL, *max = NULL;
-    int max_len = 0, min_len = 0;
-    int carry = 0;
-    int m = 0, n = 0;
+    int carry = 0, m = 0, n = 0;
+    int max_len = a_len > b_len ? a_len : b_len;
 
-    if (a_len > b_len) {
-        max_len = a_len;
-        max = a;
-        min_len = b_len;
-        min = b;
-    } else {
-        max_len = b_len;
-        max = b;
-        min_len = a_len;
-        min = a;
-    }
+    char *target = malloc((max_len + 2) * sizeof(char));
+    memset(target, 0, (max_len + 2) * sizeof(char));
 
-    static char target[100000] = {0};
-    memset(target, 0, sizeof(target));
-
-    for (int i = 0; i <= max_len; i++) {
-        carry = (n + m + carry) / 2;
-        n = (i < min_len) ? min[min_len - i - 1] - '0' : 0;
-        m = (i < max_len) ? max[max_len - i - 1] - '0' : 0;
-        target[max_len - i] = (n + m + carry) % 2 + '0';
-        printf("m:%d, n:%d, target[%d]: %c, carry:%d\r\n", m, n, max_len - i, target[max_len - i], carry);
+    while (max_len >= 0) {
+        //carry = (n + m + carry) / 2;
+        carry = (n + m + carry) >> 1;
+        n = (b_len) ? b[--b_len] - '0' : 0;
+        m = (a_len) ? a[--a_len] - '0' : 0;
+        //target[max_len--] = (n + m + carry) % 2 + '0';
+        target[max_len--] = ((n + m + carry) & 1) + '0';
+        //printf("m:%d, n:%d, target[%d]: %c, carry:%d\r\n", m, n, max_len + 1, target[max_len + 1], carry);
     }
     return target + (1 - carry);
 }
