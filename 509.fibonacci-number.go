1eb52816e1bbd42b04afcba2fa8e1d4ac1bb3698
adgiczone <adgiczone@gmail.com>
2024-05-29 16:00:13 +0800

    add 509

--- 509.fibonacci-number.go
+++ 509.fibonacci-number.go
@@ -6,7 +6,21 @@
 
 // @lc code=start
 func fib(n int) int {
-    
+	if n == 0 {
+		return 0
+	}
+	dp := make([]int, n+1)
+	// base case
+	dp[0] = 0
+	dp[1] = 1
+	if n < 2 {
+		return dp[n]
+	}
+	//状态转移方程
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
 }
-// @lc code=end
 
+// @lc code=end
