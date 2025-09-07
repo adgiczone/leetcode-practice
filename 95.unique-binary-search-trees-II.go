15da2ba90878a5c88dd70cfd9704bf8465621fe6
adgiczone <adgiczone@gmail.com>
2025-09-07 20:34:54 +0800

    add 95

---
+++ 95.unique-binary-search-trees-II.go
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=95 lang=golang
+ * @lcpr version=30202
+ *
+ * [95] Unique Binary Search Trees II
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func generateTrees(n int) []*TreeNode {
+
+}
+
+// @lc code=end
+
+/*
+// @lcpr case=start
+// 3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// 1\n
+// @lcpr case=end
+
+*/
+
